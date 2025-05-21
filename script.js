a91d343923fc011995a6820074d30dca10307253
nerdynischal <nerdynischal@gmail.com>
2025-05-21 16:49:13 +0100

    Create basic math functions

---
+++ script.js
@@ -0,0 +1,24 @@
+let x = "";
+let y = "";
+let operator = "";
+
+function add(x, y) {
+  return x + y;
+}
+
+function subtract(x, y) {
+  return x - y;
+}
+
+function multiply(x, y) {
+  return x * y;
+}
+
+function divide(x, y) {
+  return x / y;
+}
+
+console.log(add(4, 5));
+console.log(subtract(5, 4));
+console.log(multiply(5, 4));
+console.log(divide(4, 2));
